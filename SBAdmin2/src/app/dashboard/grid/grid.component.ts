181c4a748465da7d50799ff7a561d929c4ea73ad
Hulva Luva.H <Luva.L.Hua@newegg.com>
2018-03-24 13:34:45 +0800

    regular commit

--- SBAdmin2/src/app/dashboard/grid/grid.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-    selector: 'app-grid-cmp',
-    templateUrl: './grid.component.html'
-})
-
-export class GridComponent {}
--- SBAdmin2/src/app/login/login.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './index';
-
-export const LoginRoutes: Routes = [
-    {
-      path: '', component: LoginComponent
-    }
-];
