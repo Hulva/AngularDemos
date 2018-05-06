1072a9b01fc2285f106615e55ca35a18b4a5ec3b
Hulva Luva.H <lvsweet217@163.com>
2018-05-06 10:23:02 +0800

    muuri demo

--- ng-practice/src/app/home/home.component.ts
+++ ng-practice/src/app/home/home.component.ts
@@ -27,8 +27,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() {
     this.demos = [];
-    this.demos.push(new Demo(Date.now(), 'muuri', 'Muuri Demo'));
-    this.demos.push(new Demo(Date.now(), 'Demo1', 'Demo1'));
+    this.demos.push(new Demo(Date.now(), '/muuri', 'Muuri Demo'));
+    this.demos.push(new Demo(Date.now(), '/Demo1', 'Demo1'));
     console.log(this.demos);
   }
 
@@ -67,7 +67,15 @@ export class HomeComponent implements OnInit, AfterViewInit {
         dragSortInterval: 0,
         dragContainer: document.body,
         dragReleaseDuration: 400,
-        dragReleaseEasing: 'ease'
+        dragReleaseEasing: 'ease',
+        // visibleStyles: {
+        //   opacity: 1,
+        //   transform: 'rotate(45deg)'
+        // },
+        // hiddenStyles: {
+        //   opacity: 0,
+        //   transform: 'rotate(-45deg)'
+        // }
       })
       .on('dragStart', (item) => {
         this.dragCounter = this.dragCounter + 1;
